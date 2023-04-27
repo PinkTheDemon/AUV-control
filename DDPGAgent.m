a768b622162f37c36af4ef4319640f315776e22f
PinkTheDemon <1929833884@qq.com>
2023-04-27 17:01:52 +0800

    20230427 CLF放松问题

--- DDPGAgent.m
+++ DDPGAgent.m
@@ -11,6 +11,8 @@ obsInfo.Description = 'w, q, z, theta';
 actInfo = rlNumericSpec([4 1]); % 2 for CLF and 2 for CBF
 actInfo.Name = 'compensate term';
 actInfo.Description = 'alpha(*u), beta';
+actInfo.LowerLimit = -1e3*ones(4,1);
+actInfo.UpperLimit =  1e3*ones(4,1);
 
 mainPath = [
     featureInputLayer(4, Name='obsInLyr')
@@ -40,31 +42,39 @@ actorNet = [
     fullyConnectedLayer(400)
     reluLayer
     fullyConnectedLayer(4)
+%     tanhLayer
+%     scalingLayer(Scale=1e3*ones(4,1),Bias=zeros(4,1))
     ];
 
 actorNet = dlnetwork(actorNet);
 actor = rlContinuousDeterministicActor(actorNet,obsInfo,actInfo);
-
 criticOptions = rlOptimizerOptions( ...
     LearnRate = 1e-3, ...
-    GradientThreshold = 10, ...
-    L2RegularizationFactor = 1e-4);
+    GradientThreshold = 1, ...
+    L2RegularizationFactor = 1e-2);
 actorOptions = rlOptimizerOptions( ...
     LearnRate = 1e-4, ...
-    GradientThreshold = 10, ...
-    L2RegularizationFactor = 1e-4);
+    GradientThreshold = 1, ...
+    L2RegularizationFactor = 1e-2);
+% 增大正则化参数，减小buffer大小
 
-agentOptions = rlDDPGAgentOptions( ...
+agentOptions = rlTD3AgentOptions( ...
     SampleTime = Ts, ...
     ActorOptimizerOptions = actorOptions, ...
     CriticOptimizerOptions = criticOptions, ...
-    ExperienceBufferLength = 3e2);
-agentOptions.NoiseOptions.Variance = [0.1;0.1;0.1;0.1];
-agentOptions.NoiseOptions.VarianceDecayRate = 5e-3;
+    ExperienceBufferLength = 1e4);
+% agentOptions.ExplorationModel.StandardDeviation = [0.1; 0.1; 0.2; 0.2];
+% agentOptions.ExplorationModel.StandardDeviationDecayRate = 1e-3;
+agent = rlTD3Agent(actor,critic,agentOptions);
 
-agent = rlDDPGAgent(actor,critic,agentOptions);
+% % 加载预训练模型
+% load("TD3_Model.mat");
+% % % 设置 Actor 网络为预训练模型
+% % agent.Actor = loadedActorNetwork;
+% % % 设置 Critic 网络为预训练模型
+% % agent.Critic = loadedActorNetwork;
 
-maxepisodes = 2e2;
+maxepisodes = 5e2;
 maxsteps = ceil(Tf/Ts);
 trainingOpts = rlTrainingOptions( ...
     MaxEpisodes = maxepisodes, ...
@@ -72,7 +82,7 @@ trainingOpts = rlTrainingOptions( ...
     Verbose = false, ...
     Plots = "training-progress", ...
     StopTrainingCriteria = 'EpisodeCount', ...
-    StopTrainingValue = 150);
+    StopTrainingValue = 500);
 % trainingOpts.UseParallel = true;
 % trainingOpts.ParallelizationOptions.Mode = 'async';
 % trainingOpts.ParallelizationOptions.StepsUntilDataIsSent = 32;
@@ -83,3 +93,4 @@ if dotraining
     trainingStats = train(agent, env, trainingOpts);
 end
 generatePolicyFunction(agent);
+save('TD3_Model1.mat', 'agent');
